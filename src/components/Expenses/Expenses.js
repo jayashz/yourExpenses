899a57372833ede827b3a87b3064571b4d0171b5
jayashz <jayzstha15@gmail.com>
2023-08-27 14:27:21 +0545

    made the filter by year function functioning

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,19 +2,34 @@ import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
+import { useState } from 'react';
+
+
 function Expenses(props) {
+  
+  const [filteredYear,setFilteredYear] = useState('2023');
+
+  //This sets the state to be filtered as
   const filterActivate=(filterYear)=>{
-      console.log(filterYear);
+    setFilteredYear(filterYear);
   };
+
+  //This filters the Expense list based on the selected year
+  const filteredExpense = props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear;
+  });
   return (
     <Card className="expenses">
-      <ExpenseFilter onChangeFilter={filterActivate} />
-      {props.items.map((expense)=>(
+      <ExpenseFilter  seleted={filteredYear}
+       onChangeFilter={filterActivate} />
+       {filteredExpense.length===0 ? (<p>No expenses found.</p>): filteredExpense.map((expense)=>(
         <ExpenseItem
+        key={expense.id}
         title={expense.title}
         amount={expense.amount}
         date={expense.date}/>
-    ))};
+    )) }
+
       
     </Card>
   );
